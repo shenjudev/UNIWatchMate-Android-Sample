7fda575111065364707e53f797896e6127d98f54
wangrui <Ray>
2023-11-02 12:25:03 +0800

    refactor code 2

--- lib-chuanyin/lib-sj-sdk/src/main/java/com/sjbt/sdk/entity/MsgBean.kt
+++ lib-chuanyin/lib-sj-sdk/src/main/java/com/sjbt/sdk/entity/MsgBean.kt
@@ -1,42 +1,29 @@
-package com.sjbt.sdk.entity;
+package com.sjbt.sdk.entity
 
-import static com.sjbt.sdk.SJConfigKt.TAG_SJ;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_8001;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_8002;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_8003;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_8004;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_800D;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_800F;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.CMD_ID_802E;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.HEAD_CAMERA_PREVIEW;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.HEAD_COMMON;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.HEAD_FILE_SPP_A_2_D;
-import static com.sjbt.sdk.spp.cmd.CmdConfigKt.HEAD_NODE_TYPE;
-import android.util.Log;
-import com.sjbt.sdk.utils.BtUtils;
+import android.util.Log
+import com.sjbt.sdk.TAG_SJ
+import com.sjbt.sdk.spp.cmd.*
+import com.sjbt.sdk.utils.BtUtils
 
-public class MsgBean {
-    //    public String head;
-    public byte head;
-    public int cmdOrder;
-    public String cmdIdStr;
-    public int cmdId;
+class MsgBean {
+    @JvmField
+    var head: Byte = 0
+    var cmdOrder = 0
+    var cmdIdStr: String? = null
+    @JvmField
+    var cmdId = 0
+    var divideType: Byte = 0
+    var payloadPackTotalLen: Short = 0
+    var payloadLen = 0
+    var offset = 0
+    var crc = 0
+    var divideIndex = 0
+    lateinit var originData: ByteArray
+    lateinit var payload: ByteArray
 
-    public byte divideType;
-    public short payloadPackTotalLen;
-    public int payloadLen;
+    var payloadJson: String? = null
 
-    public int offset;
-    public int crc;
-    public int divideIndex;
-
-    public byte[] payload;
-    public String payloadJson;
-
-    public byte[]  originData;
-
-    @Override
-    public String toString() {
+    override fun toString(): String {
         return "BiuMsgBean{" +
                 "head=" + head +
                 ", cmdOrder=" + cmdOrder +
@@ -45,52 +32,41 @@ public class MsgBean {
                 ", payloadLen=" + payloadLen +
                 ", offset=" + offset +
                 ", crc=" + crc +
-                '}';
+                '}'
     }
 
-    public Boolean isNotTimeOut() {
-        return (head == HEAD_COMMON && cmdId == CMD_ID_800D)//绑定
-                || (head == HEAD_COMMON && cmdId == CMD_ID_800F)//我的表盘列表
-                || (head == HEAD_COMMON && cmdId == CMD_ID_802E)//绑定
+    //绑定
+    //我的表盘列表
+    //绑定
+    //传输文件的过程中，采用连续传输的方式
+    //相机预览
+    //                || (head == HEAD_NODE_TYPE && cmdId == CMD_ID_8001)//节点消息
+    //                || (head == HEAD_NODE_TYPE && cmdId == CMD_ID_8002)//节点消息
+    //通讯层节点消息
+    val isNotTimeOut: Boolean
+        get() = (head == HEAD_COMMON && cmdId == CMD_ID_800D.toInt() || head == HEAD_COMMON && cmdId == CMD_ID_800F.toInt() || head == HEAD_COMMON && cmdId == CMD_ID_802E.toInt() || head == HEAD_FILE_SPP_A_2_D && cmdId == CMD_ID_8003.toInt() || head == HEAD_CAMERA_PREVIEW) && cmdId == CMD_ID_8002.toInt() || head == HEAD_NODE_TYPE && cmdId == CMD_ID_8004.toInt() //通讯层节点消息
 
-                || (head == HEAD_FILE_SPP_A_2_D && cmdId == CMD_ID_8003)//传输文件的过程中，采用连续传输的方式
+    private val isNodeMsg: Boolean = head == HEAD_NODE_TYPE
 
-                || (head == HEAD_CAMERA_PREVIEW && cmdId == CMD_ID_8002)//相机预览
-
-//                || (head == HEAD_NODE_TYPE && cmdId == CMD_ID_8001)//节点消息
-//                || (head == HEAD_NODE_TYPE && cmdId == CMD_ID_8002)//节点消息
-                || (head == HEAD_NODE_TYPE && cmdId == CMD_ID_8004)//通讯层节点消息
-                ;
-    }
-
-    public Boolean isNodeMsg() {
-        return head == HEAD_NODE_TYPE;
-    }
-
-    public String getTimeOutCode() {
-        String timeOutCode;
-
-        if (isNodeMsg()) {
-            short requestId = 0;
-
-            byte[] requestArray = new byte[2];
-
-            if (payload != null && payload.length > 2) {
-                requestArray[0] = payload[0];
-                requestArray[1] = payload[1];
-                requestId = BtUtils.byte2short(requestArray);
+    //节点消息的
+    val timeOutCode: String
+        get() {
+            val timeOutCode: String
+            if (isNodeMsg) {
+                var requestId: Short = 0
+                val requestArray = ByteArray(2)
+                if (payload != null && payload!!.size > 2) {
+                    requestArray[0] = payload!![0]
+                    requestArray[1] = payload!![1]
+                    requestId = BtUtils.byte2short(requestArray)
+                }
+                Log.e(TAG_SJ, "node timeout code requestId:$requestId")
+                Log.e(TAG_SJ, "node timeout code not timeout:" + isNotTimeOut)
+                timeOutCode = "" + head + requestId //节点消息的
+            } else {
+                timeOutCode = "" + head + cmdOrder + cmdId
+                Log.e(TAG_SJ, "old timeout code:$timeOutCode cmdId:$cmdId")
             }
-
-            Log.e(TAG_SJ, "node timeout code requestId:" + requestId);
-            Log.e(TAG_SJ, "node timeout code not timeout:" + isNotTimeOut());
-
-            timeOutCode = "" + head + requestId;//节点消息的
-        } else {
-            timeOutCode = "" + head + cmdOrder + cmdId;
-            Log.e(TAG_SJ, "old timeout code:" + timeOutCode+" cmdId:"+cmdId);
+            return timeOutCode
         }
-
-        return timeOutCode;
-    }
-
 }
