8114d246a9166159666cbf1b4659bab8c4b84254
wangrui <Ray>
2023-12-06 09:49:04 +0800

    fix time out exception

--- lib-chuanyin/lib-interface/src/main/java/com/base/sdk/exception/WmTimeOutException.kt
+++ lib-chuanyin/lib-interface/src/main/java/com/base/sdk/exception/WmTimeOutException.kt
@@ -3,4 +3,4 @@ package com.base.sdk.exception
 /**
  * 表示设备消息超时
  */
-class WmTimeOutException(msg: String) : WmException()
+class WmTimeOutException(val msg: String) : WmException(msg)
--- lib-chuanyin/lib-sj-sdk/src/main/java/com/sjbt/sdk/SJConfig.kt
+++ lib-chuanyin/lib-sj-sdk/src/main/java/com/sjbt/sdk/SJConfig.kt
@@ -9,7 +9,7 @@ const val MSG_INTERVAL = 15
 const val MSG_INTERVAL_FRAME: Long = 15
 const val MSG_INTERVAL_SLOW = 40
 
-const val ALARM_NAME_LEN = 60
+const val ALARM_NAME_LEN = 20
 const val ALARM_LEN = ALARM_NAME_LEN + 5 //name len + id hour minute second enable
 const val BT_ADDRESS: String = "bt_mac"
 const val DEVICE_MANUFACTURER_CODE = 0xA1
